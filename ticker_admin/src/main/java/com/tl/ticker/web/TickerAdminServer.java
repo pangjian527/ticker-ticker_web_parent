842f7c697f84a855501e30ba45ecc5d3e4f29725
pangjian <pangjian@91htw.com>
2016-12-27 11:11:51 +0800

    fix

--- ticker_admin/src/main/java/com/tl/ticker/web/TickerAdminServer.java
+++ ticker_admin/src/main/java/com/tl/ticker/web/TickerAdminServer.java
@@ -1,14 +1,18 @@
 package com.tl.ticker.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by pangjian on 16-11-30.
  */
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
 public class TickerAdminServer implements EmbeddedServletContainerCustomizer{
 
     public static void main(String[] args ){
